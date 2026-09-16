ade5b495c5177e5fbc2605e998ce7b65597f662e
Elisa Rumeysa Solberg <68196918+ElisaRumSolberg@users.noreply.github.com>
2026-09-16 10:59:47 +0200

    oppgave 2

--- src/main/java/HamburgerBrett.java
+++ src/main/java/HamburgerBrett.java
@@ -1,2 +1,81 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class HamburgerBrett {
+
+    private Queue<Hamburger> brett;
+    private int kapasitet;
+    private int nesteNummer = 1;
+
+    public HamburgerBrett(int kapasitet) {
+        this.kapasitet = kapasitet;
+        this.brett = new LinkedList<>();
+    }
+
+
+    public synchronized void leggPa(String kokkNavn) {
+
+        while (brett.size() >= kapasitet) {
+
+            System.out.println(
+                    kokkNavn
+                            + " (kokk) klar med hamburger, men brett fullt. Venter!"
+            );
+
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+
+        Hamburger hamburger = new Hamburger(nesteNummer);
+        nesteNummer++;
+
+        brett.add(hamburger);
+
+        System.out.println(
+                kokkNavn
+                        + " (kokk) legger på hamburger "
+                        + hamburger
+                        + ". Brett: "
+                        + brett
+        );
+
+        notifyAll();
+    }
+
+
+    public synchronized Hamburger taAv(String servitorNavn) {
+
+        while (brett.isEmpty()) {
+
+            System.out.println(
+                    servitorNavn
+                            + " (servitør) ønsker å ta hamburger, men brett tomt. Venter!"
+            );
+
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return null;
+            }
+        }
+
+        Hamburger hamburger = brett.poll();
+
+        System.out.println(
+                servitorNavn
+                        + " (servitør) tar av hamburger "
+                        + hamburger
+                        + ". Brett: "
+                        + brett
+        );
+
+        notifyAll();
+
+        return hamburger;
+    }
 }
--- src/main/java/Oppg2.java
+++ src/main/java/Oppg2.java
@@ -1,2 +1,44 @@
 public class Oppg2 {
+
+    public static void main(String[] args) {
+
+        final int KAPASITET = 4;
+
+        HamburgerBrett brett = new HamburgerBrett(KAPASITET);
+
+        Kokk k1 = new Kokk(brett, "Anne");
+        Kokk k2 = new Kokk(brett, "Erik");
+        Kokk k3 = new Kokk(brett, "Knut");
+
+        Servitor s1 = new Servitor(brett, "Mia");
+        Servitor s2 = new Servitor(brett, "Per");
+
+        System.out.println("I denne simuleringen har vi");
+        System.out.println("3 kokker [Anne, Erik, Knut]");
+        System.out.println("2 servitører [Mia, Per]");
+        System.out.println("Kapasiteten til brettet er 4 hamburgere.");
+        System.out.println("Vi starter ...");
+
+        k1.start();
+        k2.start();
+        k3.start();
+
+        s1.start();
+        s2.start();
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        k1.interrupt();
+        k2.interrupt();
+        k3.interrupt();
+
+        s1.interrupt();
+        s2.interrupt();
+
+        System.out.println("\nSimuleringen er avsluttet.");
+    }
 }
